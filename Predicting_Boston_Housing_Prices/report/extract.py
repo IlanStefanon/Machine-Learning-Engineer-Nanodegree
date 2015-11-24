542f2cdffc4552b0611c2b7f12888573573cf670
xUbiKx <ilan.stefanon@gmail.com>
2015-11-24 01:10:43 +0100

    Delete extract.py

--- Predicting_Boston_Housing_Prices/report/extract.py
+++
@@ -1,9 +0,0 @@
-	# Get the features and labels from the Boston housing data
-	X, y = city_data.data, city_data.target
-
-	###################################
-	### Step 3. YOUR CODE GOES HERE ###
-	###################################
-	X_train, X_test, y_train, y_test  = cross_validation.train_test_split( X, y, test_size=0.1, random_state=0)
-
-	return X_train, y_train, X_test, y_test
